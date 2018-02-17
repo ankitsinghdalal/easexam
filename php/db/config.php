25ac19616822c29a196c94263f7d00d9eeaebc4a
ankit <ankitdalal@triontechnologies.com>
2018-02-17 14:09:21 +0530

    test skip push

--- php/db/config.php
+++ php/db/config.php
@@ -1,9 +1,9 @@
 <?php
 
     $server = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "exam";
+    $username = "easexam_DB";
+    $password = "#Nokia9711@";
+    $db = "easexam_DB";
 
     $connection = mysqli_connect($server, $username, $password, $db);
 
